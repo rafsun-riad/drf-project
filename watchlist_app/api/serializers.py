249063ff5e28e1ff2c9f7cc4a5cb9275937c8499
rafsun-riad <mruhaquer@gmail.com>
2024-04-22 01:44:35 +0600

    learned about nested serializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,17 +2,18 @@ from rest_framework import serializers
 from watchlist_app.models import Watchlist, StreamPlatform
 
 
-class StreamPlatformSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = StreamPlatform
-        fields = "__all__"
-
-
 class WatchlistSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Watchlist
+        fields = "__all__"
+
+
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchlistSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = StreamPlatform
         fields = "__all__"
 
 
--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('list/', views.WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', views.WatchDetailAV.as_view(), name='movie-details'),
-    path('stream/', views.StreamPlatformAV.as_view(), name='stream')
+    path('stream/', views.StreamPlatformAV.as_view(), name='stream'),
+    path('stream/<int:pk>', views.StreamPlatformDetailAV.as_view(),
+         name='stream-detail')
 ]
