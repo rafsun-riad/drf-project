734e9c75a3b0b16470bd3dab32e3d052e57dd062
rafsun-riad <mruhaquer@gmail.com>
2024-04-28 20:48:15 +0600

    working with urls

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('stream/', views.StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>', views.StreamPlatformDetailAV.as_view(),
          name='stream-detail'),
-    path('review', views.ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', views.ReviewDetail.as_view(), name='review-detail')
+    # path('review', views.ReviewList.as_view(), name='review-list'),
+    # path('review/<int:pk>', views.ReviewDetail.as_view(), name='review-detail')
+    path('stream/<int:pk>/review', views.ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>',
+         views.ReviewDetail.as_view(), name='review-detail')
+
 ]
