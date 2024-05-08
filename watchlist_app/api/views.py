7b8ccfeecd353e12eeebd741d490da3c739e61f0
rafsun-riad <mruhaquer@gmail.com>
2024-05-08 22:28:44 +0600

    learned about viewsets

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
-from rest_framework import viewsets
+# from rest_framework import viewsets
 
 from watchlist_app.models import Watchlist, StreamPlatform, Review
 from watchlist_app.api.serializers import (
