155c3e8b6b01bccf3dd745eba88a6c9a71cad9f2
rafsun-riad <mruhaquer@gmail.com>
2024-05-09 15:27:33 +0600

    working with users

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 # from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 
 from watchlist_app.models import Watchlist, StreamPlatform, Review
 from watchlist_app.api.serializers import (
@@ -13,11 +14,21 @@ from watchlist_app.api.serializers import (
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset():
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         movie = Watchlist.objects.get(pk=pk)
 
-        serializer.save(watchlist=movie)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(
+            watchlist=watchlist, review_user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!")
+
+        serializer.save(watchlist=movie, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
