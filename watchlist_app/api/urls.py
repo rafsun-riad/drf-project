eba799b9f29fd6d82ac323252e8502114700eb75
rafsun-riad <mruhaquer@gmail.com>
2024-05-12 00:33:40 +0600

    working with permissions

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -4,16 +4,16 @@ from . import views
 
 urlpatterns = [
     path('list/', views.WatchListAV.as_view(), name='movie-list'),
-    path('<int:pk>', views.WatchDetailAV.as_view(), name='movie-details'),
+    path('<int:pk>/', views.WatchDetailAV.as_view(), name='movie-details'),
     path('stream/', views.StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>', views.StreamPlatformDetailAV.as_view(),
+    path('stream/<int:pk>/', views.StreamPlatformDetailAV.as_view(),
          name='stream-detail'),
     # path('review', views.ReviewList.as_view(), name='review-list'),
     # path('review/<int:pk>', views.ReviewDetail.as_view(), name='review-detail')
-    path('stream/<int:pk>/review-create',
+    path('<int:pk>/review-create/',
          views.ReviewCreate.as_view(), name='review-create'),
-    path('stream/<int:pk>/review', views.ReviewList.as_view(), name='review-list'),
-    path('stream/review/<int:pk>',
+    path('<int:pk>/reviews/', views.ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>/',
          views.ReviewDetail.as_view(), name='review-detail')
 
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -30,6 +30,15 @@ class ReviewCreate(generics.CreateAPIView):
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie!")
 
+        if movie.number_rating == 0:
+            movie.avg_rating = serializer.validated_data['rating']
+        else:
+            movie.avg_rating = (
+                movie.avg_rating+serializer.validated_data['rating'])/2
+
+        movie.number_rating = movie.number_rating + 1
+        movie.save()
+
         serializer.save(watchlist=movie, review_user=review_user)
 
 
