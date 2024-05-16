8d479a37408ac821843cca362b4ce5f915080735
rafsun-riad <mruhaquer@gmail.com>
2024-05-17 00:30:06 +0600

    working with registration

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
+from user_app.api import views
 
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='login')
+    path('login/', obtain_auth_token, name='login'),
+    path('register/', views.registration_view, name='register'),
 ]
