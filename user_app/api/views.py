84a4cd6e482ca14459228068dad06206ac165b3b
rafsun-riad <mruhaquer@gmail.com>
2024-05-19 15:20:39 +0600

    working with token

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from user_app.api.serializers import RegistrationSerializer
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+from user_app import models
 
 
 @api_view(['POST',])
@@ -9,6 +12,20 @@ def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
 
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+
+            data['response'] = "Registration successful"
+
+        else:
+            data = serializer.errors
+
+        return Response(data)
