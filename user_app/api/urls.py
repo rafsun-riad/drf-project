a6a8ef1ca8eeac6fb8f748b77a7e6405baf0f3ab
rafsun-riad <mruhaquer@gmail.com>
2024-05-20 16:23:10 +0600

    worked with logout

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -6,4 +6,5 @@ from user_app.api import views
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', views.registration_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
