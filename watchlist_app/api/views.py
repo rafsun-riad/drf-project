fb0bcf1cd58bc525c6be1eacebf75d733dad74ef
rafsun-riad <mruhaquer@gmail.com>
2024-05-23 22:43:42 +0600

    cleaned a bit

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
-# from rest_framework.decorators import api_view
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
-# from rest_framework import viewsets
+
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
