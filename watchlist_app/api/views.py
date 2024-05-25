dcf4aca7743eee4296c6fac15bed4c6e93511452
rafsun-riad <mruhaquer@gmail.com>
2024-05-25 22:15:56 +0600

    working fine

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,9 +1,7 @@
 from rest_framework.response import Response
-
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
-
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
@@ -141,48 +139,3 @@ class WatchDetailAV(APIView):
         movie = Watchlist.objects.get(pk=pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# @api_view(['GET', 'POST'])
-# def movie_list(request):
-
-#     if request.method == 'GET':
-#         try:
-#             movies = Movie.objects.all()
-#         except Movie.DoesNotExist:
-#             return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
-
-#         serializer = MovieSerializer(movies, many=True)
-#         return Response(serializer.data)
-
-#     if request.method == 'POST':
-#         serializer = MovieSerializer(data=request.data)
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def movie_details(request, pk):
-
-#     if request.method == 'GET':
-#         movie = Movie.objects.get(pk=pk)
-#         serializer = MovieSerializer(movie)
-#         return Response(serializer.data)
-
-#     if request.method == 'PUT':
-#         movie = Movie.objects.get(pk=pk)
-#         serializer = MovieSerializer(movie, data=request.data)
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors)
-
-#     if request.method == 'DELETE':
-#         movie = Movie.objects.get(pk=pk)
-#         movie.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
